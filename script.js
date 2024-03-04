6acb45063915c895d46d332740b85f7f91d050e4
Asif20772 <asifahmadkhan3831@gmail.com>
2024-03-04 22:30:11 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var name = 'Asif'
+var age = '52'
+var percentage = '80%'
+var StudentofACI = true
+var discount = 0.6
+var fees = 189000
+var rectangle_width = 30
+var rectangle_height = 20
+var circle_radius = 50
+var triangle_height = 20
+var triangle_base = 10
+var fahrenheit = 98
+var celcius = 25
+
+console.log('My Student Information');
+console.log('My name is',name);
+console.log('My age is',age);
+console.log('My percentage is',percentage);
+console.log('I am astudent of ACI',StudentofACI)
+
+
+Message = `
+My Student Information
+My name is ${name}
+My age is ${age}
+My percentage is ${percentage}
+My Python course fee is ${fees}
+But after discount, my fee is ${fees*discount}
+So, I am saving ${fees-(fees*discount)}
+`
+console.log(Message);
+
+
+
+
